1357650aaaa8f09ab6f6596932acc9ecbd651a2c
snehatembe <103369606+snehatembe@users.noreply.github.com>
2024-02-01 16:23:06 +0530

    Add files via upload

---
+++ ADParenthesis.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ADParenthesis {
+
+    public static List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> result = new ArrayList<>();
+
+        if (!expression.contains("+") && !expression.contains("-") && !expression.contains("*")) {
+            result.add(Integer.parseInt(expression));
+            return result;
+        }
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                List<Integer> leftResults = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> rightResults = diffWaysToCompute(expression.substring(i + 1));
+
+                for (int left : leftResults) {
+                    for (int right : rightResults) {
+                        if (c == '+') {
+                            result.add(left + right);
+                        } else if (c == '-') {
+                            result.add(left - right);
+                        } else if (c == '*') {
+                            result.add(left * right);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String expression = "2*3-4*5";
+        List<Integer> result = diffWaysToCompute(expression);
+        System.out.println(result);
+    }
+}
